e7c7b63b50719f5843fc07205fae9dd8b635b91c
Louis Aubert <louis.aubert@flamefy.com>
2020-08-05 19:53:27 +0200

    Update metadata

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import '../static/css/animate.css';
 import '../static/css/flaticon.css';
 import '../static/styles/style.scss';
 // Change based on available styles like pink, brink-pink, purple then remove comment from below import & change based on available colors in static/styles folder
-// import '../static/styles/purple-style.scss'; 
+// import '../static/styles/purple-style.scss';
 import '../static/css/responsive.scss';
 
 import { Provider } from 'react-redux';
@@ -31,14 +31,14 @@ export default withRedux(initStore)(
             return (
               <Container>
                 <DefaultSeo
-                  title="StartP - React + Next + Express Bootstrap 4 Template"
-                  description="StartP is a React and Bootstrap 4 based Professional website template. This has been built with React, ES6+ and as framework Next.js and Express.js."
+                  title="vendremaboite.fr"
+                  description="Vendre sa boite n'aura jamais été aussi simple et rapide."
                   openGraph={{
                     type: "website",
-                    locale: "en_IE",
-                    url: "https://startp-react.envytheme.com/",
+                    locale: "fr_FR",
+                    url: "https://www.vendremaboite.fr/",
                     site_name:
-                      "StartP - React + Next + Express Bootstrap 4 Template"
+                      "vendremaboite.fr"
                   }}
                 />
                 <Provider store={store}>
