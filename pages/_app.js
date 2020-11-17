47fbb5a0b3d7411a863180efc0563cd3ed468a04
Louis Aubert <louis.aubert8@gmail.com>
2020-11-17 18:46:28 +0100

    fix responsive

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import '../static/css/flaticon.css';
 import '../static/styles/style.scss';
 import '../static/styles/main-banner.scss';
 
+import '../static/styles/responsive.scss';
 // Change based on available styles like pink, brink-pink, purple then remove comment from below import & change based on available colors in static/styles folder
 // import '../static/styles/purple-style.scss';
 
